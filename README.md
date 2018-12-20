e7574fa1bcb8721cb9e84c69f4b2b3cde0475893
Nick Nisi <nick@nisi.org>
2018-12-20 10:55:03 -0600

    nicknisi-ify everything

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
-It's me, Tierney!
+It's me, Nick Nisi!
 
 # Usage
 Via npx:
 ```
-npx bitandbang
+npx nicknisi
 ```
 
 Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
+
+### Special Thanks
+
+Thanks to [Tierney Cyren](https://github.com/bnb) for this awesome idea and for the code which this repo is forked from!
--- bin/card.js
+++ bin/card.js
@@ -14,14 +14,14 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('Tierney Cyren /'),
-  handle: chalk.cyan('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  twitter: chalk.cyan('https://twitter.com/bitandbang'),
-  github: chalk.cyan('https://github.com/bnb'),
-  linkedin: chalk.cyan('https://linkedin.com/in/bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.white('npx bitandbang'),
+  name: chalk.white('Nick Nisi /'),
+  handle: chalk.cyan('nicknisi'),
+  work: chalk.white('Senior JavaScript developer at SitePen'),
+  twitter: chalk.cyan('https://twitter.com/nicknisi'),
+  github: chalk.cyan('https://github.com/nicknisi'),
+  linkedin: chalk.cyan('https://linkedin.com/in/nicknisi'),
+  web: chalk.cyan('https://nicknisi.com'),
+  npx: chalk.white('npx nicknisi'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
   labelGitHub: chalk.white.bold('    GitHub:'),
